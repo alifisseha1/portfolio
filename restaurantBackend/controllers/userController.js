7f18e79c6aa0731f76478ee1d59f4eb58eb70990
fisseha ali <47268644+alifisseha1@users.noreply.github.com>
2024-12-24 23:23:47 -0500

    Update userController.js

--- restaurantBackend/controllers/userController.js
+++ restaurantBackend/controllers/userController.js
@@ -11,8 +11,8 @@ const loginUser = async (req, res) => {
     if (!user) {
       return res.json({ success: false, message: "User Doesn't exis" });
     }
-    // const isMatch = await bcrypt.compare(password, user.password);
-    const isMatch = user.password === password;
+    const isMatch = await bcrypt.compare(password, user.password);
+  
     if (!isMatch) {
       return res.json({ success: false, message: "Invalid credentials" });
     }
@@ -51,14 +51,14 @@ const registerUser = async (req, res) => {
       });
     }
     // hashing user password
-    // const salt = await bcrypt.genSalt(10);
-    // const hashedPassword = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new userModel({
       name: name,
       email: email,
-      // password: hashedPassword,
-      password: password,
+      password: hashedPassword,
+      
       
     });
 
